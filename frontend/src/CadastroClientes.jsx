37262da3f5513777cd6a375ab355aac8a7c09b78
Rogerio-Lacerda <rogerio.barrosg@gmail.com>
2025-03-24 00:20:25 -0300

    Criando pastas separadas para o backend e o frontend e fazendo eles funcionarem juntos

--- frontend/src/CadastroClientes.jsx
+++ frontend/src/CadastroClientes.jsx
@@ -8,27 +8,56 @@ const CadastroClientes = () => {
   const [form, setForm] = React.useState({
     nome: '',
     email: '',
-    telefone: '',
+    celular: '',
     senha: '',
   });
 
   const handleChange = ({ target }) => {
     const { id, value } = target;
     setForm({ ...form, [id]: value });
-    console.log(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Impede o comportamento padrão do formulário (recarregar a página)
+
+    try {
+      const response = await fetch(
+        'http://127.0.0.1:8888/api/clientes/cadastro',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form),
+        },
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Aqui você pode exibir uma mensagem de sucesso ou redirecionar o usuário
+        console.log('Cliente cadastrado com sucesso:', data);
+      } else {
+        // Exibe mensagem de erro, caso o cadastro falhe
+        console.error('Erro no cadastro:', data);
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+    }
   };
 
   return (
     <>
       <Header />
       <div className={styles.cadastro}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <h2>Cadastro Clientes</h2>
           <Input
             id="nome"
             label="Nome"
             value={form.nome}
             onChange={handleChange}
+            type="name"
             required
           />
           <Input
@@ -36,13 +65,15 @@ const CadastroClientes = () => {
             label="Email"
             value={form.email}
             onChange={handleChange}
+            type="email"
             required
           />
           <Input
-            id="telefone"
-            label="Telefone"
-            value={form.telefone}
+            id="celular"
+            label="Celular"
+            value={form.celular}
             onChange={handleChange}
+            type="tel"
             required
           />
           <Input
@@ -50,6 +81,7 @@ const CadastroClientes = () => {
             label="Senha"
             value={form.senha}
             onChange={handleChange}
+            type="password"
             required
           />
           <Button texto="Cadastrar" className={styles.button} />
