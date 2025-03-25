d935061bc381d4c163c8be328651dccee0acb5fb
gabriellyvenancio <gabrielly.anjos@luminae.com.br>
2025-03-25 09:00:22 -0300

    Adicionando rotas barbeiro

---
+++ backend/Routes/barber.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, Flask,request, jsonify
+from config.config import app
+from Controller.barbeiroController import criar_barbeiro, get_barbeiro
+
+barbeiro_blueprint = Blueprint('barbeiro', __name__, url_prefix='/api')
+
+@barbeiro_blueprint.route(('/barbeiro/cadastro/<int:idBarbeiro>'), methods=['POST'])
+def cadastro(idBarbeiro):
+    forms_cadastro = request.get_json()
+    barbeiro = criar_barbeiro(forms_cadastro,idBarbeiro)
+    return jsonify(barbeiro), barbeiro['status_code']
+
+@barbeiro_blueprint.route(('/barbeiro/cadastro/<int:idBarbeiro>'), methods=['GET'])
+def consulta(idBarbeiro):
+    barbeiro = get_barbeiro(idBarbeiro)
+    return jsonify(barbeiro), barbeiro['status_code']
+
+app.register_blueprint(barbeiro_blueprint)
+
