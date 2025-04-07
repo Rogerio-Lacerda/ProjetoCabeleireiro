b268f287af6c4535b908d9ba04dd6cf53c2ee818
gyovanna lima <gyovannalima01@gmail.com>
2025-04-06 22:31:02 -0300

    voltando arquivo para o import correto

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../css/components/Input.module.css';
+import styles from '../css/components/Error.module.css';
 
 const Error = ({ texto }) => {
   return <p className={styles.error}>{texto}</p>;
